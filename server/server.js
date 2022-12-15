c5521a52b37cee626fbf0f364fed4d261455212f
LaLacou <108777796+llacourciere@users.noreply.github.com>
2022-12-14 18:40:49 -0800

    schemas update

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const {ApolloServer} = require('apollo-server-express');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
-const { typeDefs, resolvers } = require('./Schemas')
+const { typeDefs, resolvers } = require('./schemas')
 
 const app = express();        
 const PORT = process.env.PORT || 3001;
